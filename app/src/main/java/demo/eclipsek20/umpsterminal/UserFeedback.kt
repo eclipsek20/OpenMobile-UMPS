272838ea1307e20b7bb12c9c105db9870d980f6a
eclipsek <61328707+eclipsek20@users.noreply.github.com>
2025-05-25 01:14:25 +0200

    R Code logic refactor

--- app/src/main/java/demo/eclipsek20/umpsterminal/UserFeedback.kt
+++ app/src/main/java/demo/eclipsek20/umpsterminal/UserFeedback.kt
@@ -1,6 +1,6 @@
 package demo.eclipsek20.umpsterminal
 
-import demo.eclipsek20.umpsterminal.R.raw
+import demo.eclipsek20.umpsterminal.R
 import android.content.Context
 import android.media.MediaPlayer
 import android.os.VibrationEffect
@@ -10,13 +10,13 @@ import android.os.Vibrator
 fun playSinglePing(context: Context) {
     val vibrator = context.getSystemService(Vibrator::class.java)
     vibrator?.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
-    val mp: MediaPlayer = MediaPlayer.create(context, raw.notification_decorative_02)
+    val mp: MediaPlayer = MediaPlayer.create(context, R.raw.notification_decorative_02)
     mp.start()
 }
 
 fun playDoublePing(context: Context) {
     val vibrator = context.getSystemService(Vibrator::class.java)
     vibrator?.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
-    val mp: MediaPlayer = MediaPlayer.create(context, raw.notification_decorative_01)
+    val mp: MediaPlayer = MediaPlayer.create(context, R.raw.notification_decorative_01)
     mp.start()
 }

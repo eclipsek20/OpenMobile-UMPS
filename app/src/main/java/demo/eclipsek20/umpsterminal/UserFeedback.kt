624d0f56f614785023aca6fd419af50345c90f6e
eclipsek <61328707+eclipsek20@users.noreply.github.com>
2025-05-25 01:19:32 +0200

    Improve vibrator logic

--- app/src/main/java/demo/eclipsek20/umpsterminal/UserFeedback.kt
+++ app/src/main/java/demo/eclipsek20/umpsterminal/UserFeedback.kt
@@ -8,14 +8,18 @@ import android.os.Vibrator
 
 fun playSinglePing(context: Context) {
     val vibrator = context.getSystemService(Vibrator::class.java)
-    vibrator?.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
+    val mVibratePattern = longArrayOf(0, 200)
+    val effect = VibrationEffect.createWaveform(mVibratePattern, -1)
+    vibrator?.vibrate(effect)
     val mp: MediaPlayer = MediaPlayer.create(context, R.raw.notification_decorative_02)
     mp.start()
 }
 
 fun playDoublePing(context: Context) {
     val vibrator = context.getSystemService(Vibrator::class.java)
-    vibrator?.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
+    val mVibratePattern = longArrayOf(0, 150, 100, 150)
+    val effect = VibrationEffect.createWaveform(mVibratePattern, -1)
+    vibrator?.vibrate(effect)
     val mp: MediaPlayer = MediaPlayer.create(context, R.raw.notification_decorative_01)
     mp.start()
 }

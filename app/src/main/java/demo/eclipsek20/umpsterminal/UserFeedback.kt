5bc91d47b36cadc670700f69e464473a0f762287
eclipsek <61328707+eclipsek20@users.noreply.github.com>
2025-05-25 01:13:21 +0200

    Implemented haptic feedback

--- app/src/main/java/demo/eclipsek20/umpsterminal/UserFeedback.kt
+++ app/src/main/java/demo/eclipsek20/umpsterminal/UserFeedback.kt
@@ -3,14 +3,20 @@ package demo.eclipsek20.umpsterminal
 import demo.eclipsek20.umpsterminal.R.raw
 import android.content.Context
 import android.media.MediaPlayer
+import android.os.VibrationEffect
+import android.os.Vibrator
 
 
 fun playSinglePing(context: Context) {
+    val vibrator = context.getSystemService(Vibrator::class.java)
+    vibrator?.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
     val mp: MediaPlayer = MediaPlayer.create(context, raw.notification_decorative_02)
     mp.start()
 }
 
 fun playDoublePing(context: Context) {
+    val vibrator = context.getSystemService(Vibrator::class.java)
+    vibrator?.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
     val mp: MediaPlayer = MediaPlayer.create(context, raw.notification_decorative_01)
     mp.start()
 }
